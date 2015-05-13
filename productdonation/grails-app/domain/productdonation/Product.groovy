3b0ecb8e50c7d3adaa26642c77832014ffa153be
Quan <Quan@Quan-PC>
2015-05-13 11:14:07 +0700

    commit code

--- productdonation/grails-app/domain/productdonation/Product.groovy
+++ productdonation/grails-app/domain/productdonation/Product.groovy
@@ -6,12 +6,15 @@ import productdonation.util.DateUtil;
 
 class Product {
 	static belongsTo=[user:User,category:Category] 
+	transient springSecurityService
 	String productName
+	byte[] photo
 	String description
 	String summary
 	String tag
 	String address
-	boolean isDonation;
+	String contactDetail
+	boolean isDonation
 	boolean status
 	Date createdDate
 	Date activeDate
@@ -21,6 +24,7 @@ class Product {
 		tag blank:false, nullable:false, inList:["Usuario", "Hoy por ti"]
 		productName blank:false, nullable:false
 		description blank:false, nullable:false
+		contactDetail blank:false, nullable:false
 		summary blank:false, nullable:false
 		status blank:false
 		address nullable:true
@@ -28,7 +32,7 @@ class Product {
 		activeDate blank:false, nullable:false, min:new Date()
 	}
 	static mapping = {
-		photos cascade: 'all-delete-orphan'
+	//	photos cascade: 'all-delete-orphan'
 		status defaultValue:false
 		isDonation defaultValue:true
 		views defaultValue:1
@@ -41,5 +45,6 @@ class Product {
 		if(activeDate==DateUtil.NULL_DATE){
 			activeDate=new Date();
 		}
+		user=springSecurityService.currentUser
 	}
 }
